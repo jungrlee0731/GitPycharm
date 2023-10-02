455c248e99ccdbaff0cb8d8dc1349a663ef63f4b
jungrlee0731 <96174783+jungrlee0731@users.noreply.github.com>
2023-10-02 18:09:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,3 +14,9 @@ print("Hello World 4")
 
 #adding a new value to main branch
 print("Hello World 5")
+
+#Upadtes in local master branch
+print("Hello World Updates on Local Master Branch")
+
+#Upadtes on mainbranch of Github
+print("Hello World Updates 2 on Github Only")
